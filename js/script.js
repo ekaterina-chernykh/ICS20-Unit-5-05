10e8a031a62541a817ac53c4d2f2c026dc6696c4
ekaterina-chernykh <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-12-05 21:26:21 -0500

    angles parseInt

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ function myButtonClicked() {
     ) *
     (180 / Math.PI)
 
-  let sumOfAngles = angleA + angleB + angleC
+  let sumOfAngles = parseInt(angleA) + parseInt(angleB) + parseInt(angleC)
 
   if (parseInt(sumOfAngles) == 180) {
     if (angleA == angleB && angleA == angleC) {
